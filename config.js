144d13f8e8546bf938076b8562eb7b10f133f809
yremzi300-star <yremzi300@gmail.com>
2025-12-19 19:09:33 +0300

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: "BOT_TOKENIN",
+  ticketCategory: "TICKET_CATEGORY_ID",
+  staffRole: "STAFF_ROLE_ID",
+  adminRole: "ADMIN_ROLE_ID",
+  logChannel: "LOG_CHANNEL_ID",
+  embedColor: 0x0b1c3d
+};
